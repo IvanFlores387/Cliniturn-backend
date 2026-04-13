cd1d0b5f88743ad1c02d73edf48fdfa25726c6cb
IvanFlores387 <145875422+IvanFlores387@users.noreply.github.com>
2026-04-13 00:04:50 -0600

    Incremento 4 Correccion

--- src/utils/api-response.js
+++ src/utils/api-response.js
@@ -5,7 +5,7 @@ function ok(res, data = null, message = 'Operación exitosa', status = 200) {
     data,
   });
 }
-
+//cola
 function fail(res, message = 'Error en la operación', status = 400, error = null) {
   return res.status(status).json({
     ok: false,
